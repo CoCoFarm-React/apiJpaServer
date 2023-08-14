816f134875142cc59edd94267bb720e7c1be067f
joungjaehyun <joungjaehyun1008@gmail.com>
2023-08-14 15:13:41 +0900

    favorite

---
+++ src/main/java/com/project/apiserver/boardfavorite/repository/BoardFavoriteRepository.java
@@ -0,0 +1,28 @@
+package com.project.apiserver.boardfavorite.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.project.apiserver.boardfavorite.entity.BoardFavorite;
+
+import jakarta.transaction.Transactional;
+
+public interface BoardFavoriteRepository extends JpaRepository<BoardFavorite, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO tbl_board_favorite (board_bno, member_mno) VALUES (:bno, :mno)", nativeQuery = true)
+    void insertSubscriptionNative(@Param("bno") Long bno, @Param("mno") Long mno);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM tbl_board_favorite WHERE bno = :bno AND mno = :mno", nativeQuery = true)
+    void deleteSubscriptionNative(@Param("bno") Long bno, @Param("mno") Long mno);
+    
+
+    @Query(value = "SELECT  COUNT(b) FROM BoardFavorite b WHERE b.board.bno = :bno GROUP BY b.board.bno")
+    Long countSub(@Param("bno") Long bno);
+    
+}
---
+++ src/main/java/com/project/apiserver/boardfavorite/service/BoardFavoriteService.java
@@ -0,0 +1,11 @@
+package com.project.apiserver.boardfavorite.service;
+
+import jakarta.transaction.Transactional;
+
+@Transactional
+public interface BoardFavoriteService {
+    
+    void incrementFavorite(Long bno, Long mno);
+    void deleteFavorite(Long bno, Long mno);
+    Long countFavorite(Long bno);
+}
